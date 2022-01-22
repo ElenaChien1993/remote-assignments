9f9e84c668997197ee93af1b2b3b7d9a1e59b42b
Elena Chien <terrisa1993@gmail.com>
2022-01-22 19:52:25 +0800

    finished project

--- week-3/app.js
+++ week-3/app.js
@@ -1,6 +1,87 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.set('view engine', 'pug');
+app.use(express.static('public'));
+app.use(cookieParser());
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// 首頁 route
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+// getData route
+app.get('/getData', (req, res) => {
+  const { number } = req.query;
+  const template = { number };
+
+  if (number) {
+    const expres = /^[0-9]*[1-9][0-9]*$/;
+    const regex = new RegExp(expres);
+    if (number.match(regex)) {
+      errMessage = '';
+      let sum = 0;
+      for (let i = 1; i <= number ; i++) {
+        sum += i;
+      }
+      template.sum = sum;
+      // res.json(template);
+    } else if (!number.match(regex)) {
+      errMessage = 'Sorry, Wrong Parameter.';
+      template.errMessage = errMessage;
+    } 
+  } else {
+    errMessage = '';
+  }
+
+  res.render('getData', template);
+})
+
+// getData JSON route
+app.get('/getData.json', (req, res) => {
+  const { number } = req.query;
+  const template = { number };
+
+  let sum = 0;
+  for (let i = 1; i <= number ; i++) {
+    sum += i;
+  }
+  template.sum = sum;
+
+  res.json(template);
+})
+
+// myName route
+app.get('/myName', (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.render('myName', { name });
+  } else {
+    res.redirect('/trackName');
+  }
+})
+
+// trackName route
+app.get('/trackName', (req, res) => {
+  const { name } = req.query;
+  if (!name) {
+    res.render('trackName');
+  } else if (name) {
+    res.cookie('username', name);
+
+    res.redirect('/myName');
+  }
+})
+
+// app.post('/trackName', (req, res) => {
+//   res.cookie('username', req.body.name);
+//   res.redirect('/myName');
+// })
+
+
 
 app.listen(3000);
---
+++ week-3/public/script.js
@@ -0,0 +1,42 @@
+const inputForm = document.getElementById('input-form');
+const inputContainer = document.getElementById('input-container');
+const inputField = document.getElementById('input-field');
+const resultContainer = document.getElementById('result-container');
+const textResultEl = document.getElementById('text-result');
+const resetBtn = document.getElementById('btn-reset');
+
+let inputNumber;
+
+const submitNumber = (e) => {
+  e.preventDefault();
+  inputNumber = e.srcElement[0].valueAsNumber;
+  if (inputNumber > 0) {
+    getData(inputNumber);
+  } else {
+    alert('Your number must be bigger than zero.')
+  }
+};
+
+const showResult = (data) => {
+  textResultEl.textContent = `Your input is: ${inputNumber}, Your sum value is: ${data}`;
+  inputContainer.classList.toggle('hidden');
+  resultContainer.classList.toggle('hidden');
+};
+
+const resetCalculator = () => {
+  inputField.value = '';
+  inputContainer.classList.toggle('hidden');
+  resultContainer.classList.toggle('hidden');
+};
+
+// API request
+async function getData (num) {
+  const url = `http://localhost:3000/getData.json?number=${num}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  
+  showResult(data.sum);
+}
+
+inputForm.addEventListener('submit', submitNumber);
+resetBtn.addEventListener('click', resetCalculator);
